2d37d3c565f3011433ff78cb749ce6567e34ad41
zunedisinambela <zunedisinambela@gmail.com>
2022-05-29 19:41:42 +0700

    Validasi

--- app/Http/Livewire/Staf.php
+++ app/Http/Livewire/Staf.php
@@ -6,6 +6,39 @@ use Livewire\Component;
 
 class Staf extends Component
 {
+    public $name;
+    public $email;
+    public $no_telp;
+    public $success;
+
+    public $rules = [
+        'name' => 'required|min:6',
+        'email' => 'required|email',
+        'no_telp' => 'required|min:12',
+    ];
+
+    protected $messages = [
+        'name.required' => 'Nama tidak boleh kosong',
+        'name.min' => 'minimal 6 karakter',
+        'email.required' => 'Email tidak boleh kosong',
+        'email.email' => 'Bukan format email',
+        'no_telp.required' => 'No telpon tidak boleh kosong',
+        'no_telp.min' => 'No telpon min 12 karakter'
+    ];
+
+    // validasi secara realtime
+    public function updated($field)
+    {
+        $this->validateOnly($field);
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $this->success = 'Data berhasil disimpan';
+    }
+
     public function render()
     {
         return view('livewire.staf')->extends('layouts.main')->section('content');
--- resources/views/livewire/staf.blade.php
+++ resources/views/livewire/staf.blade.php
@@ -1,3 +1,38 @@
 <div>
-    <h1>Ini Livewire Component Staf</h1>
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    Input Data Staf
+                </div>
+                <div class="card-body">
+                    <form wire:submit.prevent="save">
+                        <div class="form-group">
+                            <label>Nama</label>
+                            <input wire:model="name" class="form-control" value="{{ old('name') }}">
+                            @error('name')
+                                <label class="text-danger">{{ $message }}</label>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input wire:model="email" class="form-control" value="{{ old('email') }}">
+                            @error('email')
+                                <label class="text-danger">{{ $message }}</label>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label>No. Telepon</label>
+                            <input wire:model="no_telp" class="form-control" value="{{ old('no_telp') }}">
+                            @error('no_telp')
+                                <label class="text-danger">{{ $message }}</label>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                        {{ $success }}
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
